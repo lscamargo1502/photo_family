10498c7ddffda575b282c14d83221e6a410d945b
lscamargo1502 <leandro.scamar2@gmail.com>
2021-06-22 09:12:20 -0300

    Second commit

--- app/src/main/java/br/com/lscamargo/photofamily/imageAdapter.kt
+++ app/src/main/java/br/com/lscamargo/photofamily/imageAdapter.kt
@@ -2,6 +2,8 @@ package br.com.lscamargo.photofamily
 
 //import android.R
 import android.content.Context
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,12 +17,10 @@ class ImageAdapter(
     val urls: List<String>
 ): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
-    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imageView: ImageView
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        init {
-            imageView = itemView.findViewById(R.id.image_view_upload)
-        }
+        var imageView: ImageView = itemView.findViewById(R.id.image_view_upload)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
@@ -41,6 +41,8 @@ class ImageAdapter(
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         val url = urls[position]
 
+        Log.e("teste:::", url )
+
         Glide.with(holder.itemView)
             .load(url)
             .centerCrop()
@@ -48,42 +50,3 @@ class ImageAdapter(
     }
 
 }
-/*class ImageAdapter(context: Context, uploads: List<Upload>) :
-    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
-    private val mContext: Context
-    private val mUploads: List<Upload>
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val v: View = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false)
-        return ImageViewHolder(v)
-    }
-
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        val uploadCurrent: Upload = mUploads[position]
-        holder.textViewName.setText(uploadCurrent.getName())
-
-        Glide.with(mContext)
-            .load(uploadCurrent.getImageUrl())
-            .centerCrop()
-            .into(holder.imageView)
-    }
-
-    override fun getItemCount(): Int {
-        return mUploads.size
-    }
-
-    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var textViewName: TextView
-        var imageView: ImageView
-
-        init {
-            textViewName = itemView.findViewById(R.id.text_view_name)
-            imageView = itemView.findViewById(R.id.image_view_upload)
-        }
-    }
-
-    init {
-        mContext = context
-        mUploads = uploads
-    }
-}*/
--- app/src/main/java/br/com/lscamargo/photofamily/imagesActivity.kt
+++ app/src/main/java/br/com/lscamargo/photofamily/imagesActivity.kt
@@ -1,5 +1,6 @@
 package br.com.lscamargo.photofamily
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -10,15 +11,16 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.component1
 import com.google.firebase.storage.ktx.component2
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.awaitAll
+import java.util.Arrays.toString
 
 
 class imagesActivity : AppCompatActivity() {
 
-    //private val imageRef = Firebase.storage.reference
-
     val storage = Firebase.storage
     val listRef = storage.reference.child("/images")
-    //private val mAdapter: ImageAdapter? = null
+
+    private var imageAdapter: ImageAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,46 +28,33 @@ class imagesActivity : AppCompatActivity() {
 
         val mRecyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         mRecyclerView.setHasFixedSize(true)
-        mRecyclerView.setLayoutManager(LinearLayoutManager(this))
-        //mRecyclerView.adapter = mAdapter
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        mRecyclerView.adapter = imageAdapter
 
-        //var mUploads = ArrayList<String>()
-
-        //val mDatabaseRef = FirebaseStorage.getInstance().getReference("images")
-
-        //val lista = mDatabaseRef.listAll()
         val imageUrls = ArrayList<String>()
 
         listRef.listAll()
             .addOnSuccessListener { (items, prefixes) ->
                 prefixes.forEach { prefix ->
-                    // All the prefixes under listRef.
-                    // You may call listAll() recursively on them.
 
                     Log.e("LISTA!: ", prefix.path)
                     Toast.makeText(this, prefix.path, Toast.LENGTH_SHORT).show()
                 }
 
-
                 items.forEach { item ->
-                    //Log.e("ITEM!: ",item.downloadUrl.)
-
-                    val url = item.downloadUrl
-
-                    Log.e("LISTA!: ", item.toString())
-                    imageUrls.add(url.toString())
-
+                    val url = item.downloadUrl.addOnSuccessListener {
+                        Log.e("LISTA!: ", it.toString())
+                        imageUrls.add(it.toString())
+                    }
                 }
-                val imageAdapter = ImageAdapter(imageUrls)
-                mRecyclerView.adapter = imageAdapter
 
             }
             .addOnFailureListener {
                 Toast.makeText(this, "FALHOU!", Toast.LENGTH_SHORT).show()
-            // Uh-oh, an error occurred!
+                // Uh-oh, an error occurred!
             }
 
+        imageAdapter = ImageAdapter(imageUrls)
+        mRecyclerView.adapter = imageAdapter
     }
-
-
 }
